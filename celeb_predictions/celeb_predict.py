2c838f84f6bb888ca4e4ca20ae50fe10384d096e
ljkniest <ljkniest@uw.edu>
2023-05-24 13:39:41 -0700

    Create initial python files

---
+++ celeb_predictions/celeb_predict.py
@@ -0,0 +1,7 @@
+# this will handle intake of photos and spitting out of a prediction.
+# "main method"
+
+# call preprocessing file
+# train model
+# get webcam spooled
+# get webcam photo and upload
