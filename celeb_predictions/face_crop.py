dc7c0c9e4da70c1e2d775b97c739dad66508b661
audreyecraig <aecraig@uw.edu>
2023-05-24 14:10:35 -0700

    Initial implementation of face crop

--- celeb_predictions/face_crop.py
+++ celeb_predictions/face_crop.py
@@ -1,6 +1,27 @@
 # this is the face preprocessing file
+import cv2
 
-# prepare the data
-# TODO: import files like we do in the assignments
+#create a function to grab each image, detect the face, crop the face, save the face image
+def face_crop(path, scale):
+    
+    #grabs all image directory paths
+    img_list = glob.glob(path + '/*.jpg')
+    
+    #face cascade from OpenCV
+    # haar_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt.xml')
 
-# detect faces
+    # Loading the required haar-cascade xml classifier file
+    haar_cascade = cv2.CascadeClassifier('Haarcascade_frontalface_default.xml')
+    
+    for img_name in img_list:
+        img = cv2.imread(img_name)
+        faces_rect = haar_cascade.detectMultiScale(img, scaleFactor=scale, minNeighbors=5)
+        
+        # resize each face detected in images
+        for (x, y, w, h) in faces_rect:
+            face_cropped = img[y:y+h, x:x+w]
+            face_resized_img = cv2.resize(img[y:y+h, x:x+w], (175,175), interpolation = cv2.INTER_AREA)
+            
+            #save cropped face images
+            new_img_name = img_name.replace('.jpg', '')
+            cv2.imwrite(new_img_name + '.jpg', face_resized_img)
